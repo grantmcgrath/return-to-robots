6b40d761e2961a7f526559aac0ac4ea5fd445046
Grant McGrath <grantmcgrath@me.com>
2017-08-14 11:11:23 -0400

    Commiting for commitings sake.

--- apps.js
+++ apps.js
@@ -1,18 +1,29 @@
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient,
-  assert = require("assert");
+const fs = require("fs");
+const db = require("./data.js");
+const exBars = require("express-handlebars");
+const robosRoutes = require("./routes/robos.js");
+const workingRobosRoutes = require("./routes/workingRobos.js");
+const searchingRobosRoutes = require("./routes/searchingRobos.js");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
 // Connection URL
-var url = 'mongodb://localhost:27017/daily-project';
+var url = "mongodb://localhost:27017/return-to-robos";
+
+// Uses handlebars as the engine
+app.engine("handlebars", exBars());
+app.set("views", "./views");
+app.set("view engine", "handlebars");
+
+// Routes
+app.use("/", robosRoutes);
+app.use("/workingRobos", workingRobosRoutes);
+app.use("/searchingRobos", searchingRobosRoutes);
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+MongoClient.connect(url, (err, connection) => {
+  if (!err) console.log("Mongo Good");
 
-  // db.close();
-app.listen(3000, () => console.log("The magic server seems to be working.?!"));
-
-
+  app.listen(3000, () => console.log("The magic server seems to be working.?!"));
 });
