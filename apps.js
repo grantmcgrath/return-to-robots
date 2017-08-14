ca6e6d86f461c53d75d239415e388a302d36fabb
Grant McGrath <grantmcgrath@me.com>
2017-08-14 18:31:49 -0400

    Made it so each robo has their own page.

--- apps.js
+++ apps.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const fs = require("fs");
-// const db = require("./data.js");
+// const db = require("./data");
 const exBars = require("express-handlebars");
-const robosRoutes = require("./routes/robos.js");
-const workingRobosRoutes = require("./routes/workingRobos.js");
-const searchingRobosRoutes = require("./routes/searchingRobos.js");
+// const robosRoutes = require("./routes/robos");
+// const workingRobosRoutes = require("./routes/workingRobos");
+// const searchingRobosRoutes = require("./routes/searchingRobos");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 
@@ -16,13 +16,42 @@ app.engine("handlebars", exBars());
 app.set("views", "./views");
 app.set("view engine", "handlebars");
 
+// Loads static files
+app.use(express.static("css"));
+
 // Routes
-app.use("/", robosRoutes);
-app.use("/workingRobos", workingRobosRoutes);
-app.use("/searchingRobos", searchingRobosRoutes);
+// app.use("/", robosRoutes);
+// app.use("/workingRobos", workingRobosRoutes);
+// app.use("/searchingRobos", searchingRobosRoutes);
+var db;
+
+app.get("/", (req, res) => {
+  let collection = db.collection("robots");
+
+  collection.find({}).toArray(function(err, robots) {
+    res.render("home", {robots: robots});
+  });
+});
+
+app.get("/searching", (req, res) => {
+  let collection = db.collection("robots");
+
+  collection.find({}).toArray(function(err, robots) {
+    res.render("searchingRobos", {robots: robots});
+  });
+});
+
+app.get("/employed", (req, res) => {
+  let collection = db.collection("robots");
+
+  collection.find({}).toArray(function(err, robots) {
+    res.render("workingRobos", {robots: robots});
+  });
+});
 
 // Use connect method to connect to the server
 MongoClient.connect(url, (err, connection) => {
+  db = connection;
   if (!err) console.log("Mongo Good");
 
   app.listen(3000, () => console.log("The magic server seems to be working.?!"));
--- routes/robos.js
+++ routes/robos.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const routes = express.Router();
 const db = require("../data.json");
-const app = express();
 
 routes.get("/", (req, res) => {
-  var collection = db.get().collection("robots");
+  let collection = db.get().collection("robots");
 
-  collection.find({}).toArray((err, robots) => {
-    res.render("home", {returnToRobos: robots});
+  collection.find({}).toArray(function(err, robots) {
+    res.render("home", {robots: robots});
   });
 });
 
